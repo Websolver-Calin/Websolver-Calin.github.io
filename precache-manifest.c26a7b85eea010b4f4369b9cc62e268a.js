8e08c610db0c54599ef7f8413367ef41e1631c26
Websolver-Calin <58215416+Websolver-Calin@users.noreply.github.com>
2019-12-04 16:03:16 +0200

    Add files via upload

---
+++ precache-manifest.c26a7b85eea010b4f4369b9cc62e268a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c7cf747928baea71dd1a062d3867f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a082a70f9acfecf7e7a",
+    "url": "/static/css/main.6d7e17ba.chunk.css"
+  },
+  {
+    "revision": "542ad40bd58adda84182",
+    "url": "/static/js/2.9d9cd190.chunk.js"
+  },
+  {
+    "revision": "2a082a70f9acfecf7e7a",
+    "url": "/static/js/main.90028cde.chunk.js"
+  },
+  {
+    "revision": "0ad9fc5a51ce39012738",
+    "url": "/static/js/runtime-main.5da0d437.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/11.3 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "6a74e81d3a637da167a410cbb80a4cbb",
+    "url": "/static/media/shopping-cart-304843.6a74e81d.svg"
+  }
+]);
